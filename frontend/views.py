901226a8de79e300d0c72879a61ff8ff571967a5
Maxime Desmet Vanden Stock <maxime.desmet@easypost.eu>
2024-03-28 19:38:42 +0100

    matchmaking with divisions

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 from django.db.models import Case, F, FloatField, Sum, When
@@ -6,11 +7,28 @@ from django.shortcuts import render
 from elo.models import Elo
 
 
-def match_view(request):
-    total_games = Elo.objects.aggregate(total_games=Sum("n_games"))[
-        "total_games"
+def get_division():
+    ordered_records = Elo.objects.order_by("score")
+    total_records = ordered_records.count()
+    n = math.floor(total_records / 10)
+
+    total_records = ordered_records.count()
+    section_size = math.ceil(total_records / n)
+
+    section_indices = [
+        (i * section_size, (i + 1) * section_size) for i in range(n)
     ]
-    elo_with_weights = Elo.objects.annotate(
+
+    selected_section = random.randint(0, n - 1)
+    start, end = section_indices[selected_section]
+
+    return ordered_records[start:end]
+
+
+def match_view(request):
+    queryset = get_division()
+    total_games = queryset.aggregate(total_games=Sum("n_games"))["total_games"]
+    elo_with_weights = queryset.annotate(
         weight=Case(
             When(n_games__gt=0, then=float(total_games) / F("n_games")),
             default=total_games,
